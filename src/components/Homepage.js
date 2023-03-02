c5a749b35790d5027cb1f0a3a4232b3387205747
brick-pid <winphoneljb@live.com>
2023-03-02 12:46:36 +0800

    learn react context

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,11 @@
+import { UseContext } from "react";
+import MealContext from "../DataContext";
 
 function Homepage() {
   return (
-    <>
+    <div>
       <h1>Homepage</h1>
-    </>
+    </div>
   );
 }
 
