d4e4cc7c5d68052aad8da6568e48df917bdbcc88
brick-pid <winphoneljb@live.com>
2023-03-04 19:25:21 +0800

    父子通信，删除项目

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css'
+import { Meals } from './components/Homepage'
 function App () {
   return (
     <div>
       Hello World!
+      <Meals></Meals>
     </div>
   )
 }
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,10 +1,65 @@
+import React, { useState } from 'react'
 
-function Homepage() {
+function Homepage () {
   return (
     <div>
       <h1>Homepage</h1>
     </div>
-  );
+  )
 }
 
-export default Homepage;
+// Meals 把数据传给 DisplayMeals
+function Meals () {
+  const [meals, setMeals] = useState([
+    {
+      id: 1,
+      name: "Pizza",
+      description: "Cheesy goodness"
+    },
+    {
+      id: 2,
+      name: "Burger",
+      description: "Juicy goodness"
+    },
+    {
+      id: 3,
+      name: "Hot Dog",
+      description: "Sausage goodness"
+    }
+  ])
+
+  const deleteItemHandler = (id) => {
+    console.log('delete' + id)
+    const next = meals.filter(item => item.id !== id)
+    setMeals(next)
+  }
+
+  return (
+    <div>
+      {meals.map(item =>
+        <DisplayMeals
+          key={item.id}
+          name={item.name}
+          description={item.description}
+          id={item.id}
+          deleteItemHandler={deleteItemHandler} />)}
+    </div>
+  )
+}
+
+
+function DisplayMeals (props) {
+  console.log(props)
+  return (
+    <div>
+      <div>{props.name}</div>
+      <div>{props.description}</div>
+      <button onClick={() => props.deleteItemHandler(props.id)}>Delete</button>
+    </div>
+  )
+}
+
+
+export default Homepage
+
+export { Meals }
